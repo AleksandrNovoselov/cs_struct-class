577aca1daf2b0ec1f0830caa7267243362ed1d50
Novoselov_Aleksandr <9292762922@mail.ru>
2022-09-16 20:16:07 +0400

    массив готов

--- cs_struct,class/MyArray.cs
+++ cs_struct,class/MyArray.cs
@@ -8,10 +8,54 @@ namespace cs_struct_class
 {
     internal class MyArray
     {
-        private bool _result = false;
-        public int number;
+        private int _number;
+        private bool _result;
+        int _count = 0;
+        int _size = 250;
 
-        
-            
+        public void InputNumber()
+        {
+            Console.WriteLine("Дан какой-то массив целых чисел от 0 до 9." +
+                "\nВведите число и узнаем сколько раз оно встречается в массиве.");
+
+            do
+            {
+                _result = int.TryParse(Console.ReadLine(), out _number);
+
+                if (!_result)
+                    Console.WriteLine("Вводить надо цифры");
+                if (_number < 0 || _number > 9)
+                {
+                    Console.WriteLine("Вводить надо цифры от 0 до 9");
+                    _result = false;
+                }
+
+            } while (!_result);
+        }
+
+        public void FindNumber()
+        {
+            var random = new Random();
+            int[] arr = new int[_size];
+
+            for (int i = 0; i < _size; i++)
+            {
+                arr[i] = random.Next(0, 10);
+                Console.Write(arr[i] + " ");
+            }
+
+            Console.WriteLine("\nПровожу поиск...");
+
+            foreach (var item in arr)
+            {
+                if (item == _number)
+                    _count++;
+            }
+
+            System.Threading.Thread.Sleep(1000);
+
+            Console.WriteLine($"Число \"{_number}\" встречается {_count} раз");
+
+        }
     }
 }
--- cs_struct,class/Program.cs
+++ cs_struct,class/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Threading;
+
 
 namespace cs_struct_class
 {
@@ -11,53 +11,12 @@ namespace cs_struct_class
     {
         static void Main(string[] args)
         {
-            bool result;
-            int number, count=0;
+            MyArray myArray = new MyArray();
 
-            Console.WriteLine("Дан какой-то массив целых чисел от 0 до 9." +
-                "\nВведите число и узнаем сколько раз оно встречается в массиве.");
-            
-            do 
-            {
-                result = int.TryParse(Console.ReadLine(), out number);
-
-                if (!result)
-                    Console.WriteLine("Вводить надо цифры");
-                if (number < 0 || number > 9)
-                { 
-                    Console.WriteLine("Вводить надо цифры от 0 до 9");
-                    result = false;
-                }
-
-            } while (!result);
-
-            int size = 250;
-            var random = new Random();
-            int[] arr = new int[size];
-
-            for (int i = 0; i < size; i++)
-            {
-                arr[i] = random.Next(0, 10);
-            }
-            for (int i = 0; i < size ; i++)
-            {
-                Console.Write(arr[i] + " ");
-            }
-
-            Console.WriteLine("\nПровожу поиск...");
-
-            foreach (var item in arr)
-            {
-                if (item==number)
-                    count++;
-            }
-            Thread.Sleep(1000);
-
-            Console.WriteLine($"Число \"{number}\" встречается {count} раз") ;
-
+            myArray.InputNumber();
+            myArray.FindNumber();
 
             Console.ReadLine();
         }
-        
     }
 }

a15937e43c754959d72692f20f8796b0e50a7a95
Novoselov_Aleksandr <9292762922@mail.ru>
2022-09-21 19:40:29 +0400

    2 задаие с клиентами готово

---
+++ Client/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    internal class Program
+    {
+        public struct Client
+        {
+            public int idClient;
+            private static int id = 1;
+
+            public string fio;
+            public string address;
+            public int phone;
+
+            private int orders;
+            private int totalSum;
+
+            public int TotalSum
+            {
+                get => totalSum;
+                set
+                {
+                    orders++;
+                    totalSum += value;
+                }
+            }
+
+            public Client(string fio, string address, Random phone)
+            {
+                idClient = id++;
+                this.fio = fio;
+                this.address = address;
+                this.phone = phone.Next(111111, 999999);
+                orders = 0;
+                totalSum = 0;
+            }
+
+            public void Print()
+            {
+                Console.WriteLine($"Номер клиента: {idClient} ФИО: {fio}" +
+                    $" адресс: {address} тел.: {phone} Кол-во заказов: {orders} Сумма заказов: {totalSum}");
+            }
+
+            public void fillArray(ref Client[] client)
+            {
+                
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            //Описать структуру Client содержащую поля: код клиента; ФИО; адрес; телефон;
+            //количество заказов осуществленных клиентом;общая сумма заказов клиента.
+
+            int number = 0;
+            var random = new Random();
+
+            Client[] client = new Client[3];
+          
+            for (int i = 0; i < client.Length; i++)
+            {
+                client[i] = new Client("Petrov" + i, "Izhevsk", random); //заполним какими-то клиентами
+                client[i].Print();
+            }
+
+            while (number != -1)
+            {
+                Console.WriteLine("Введите номер клиента (для выхода 0)");
+                number = int.Parse(Console.ReadLine());
+                number--;
+                if (number == -1)
+                    break;
+                Console.Write("Сумма заказа: ");
+                client[number].TotalSum = int.Parse(Console.ReadLine());
+
+                foreach (var item in client)
+                {
+                    item.Print();
+                }
+            }
+
+            Console.WriteLine("Пока пока");
+            Console.ReadLine();
+        }
+    }
+}

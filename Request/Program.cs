1ee9e33899ea64edc1f84afa0806d43d57d59577
Novoselov_Aleksandr <9292762922@mail.ru>
2022-09-22 20:03:21 +0400

    последний вариант

--- Request/Program.cs
+++ Request/Program.cs
@@ -10,20 +10,24 @@ namespace Request
         public struct Request
         {
             private decimal _orderAmount;
-            public decimal OrderAmount { get => _orderAmount; set => _orderAmount = value; }
+            private int _orderCode;
+            private string _client;
+            private DateTime _date;
 
-            public int orderCode;
-            public string client;
-            public DateTime date;
+            public decimal OrderAmount { get => _orderAmount; set => _orderAmount = value; }
+            public int OrderCode { get => _orderCode; set => _orderCode = value; }
+            public string Client { get => _client; set => _client = value; }
+            public DateTime Date { get => _date; set => _date = value; }
+
             public string[] products;
 
             public void Print()
             {
-                Console.WriteLine($"Номер заказа: {orderCode} \nклиент: {client}" +
-                    $" \nдата заказа: {date.ToShortDateString()} \nСумма заказа: {_orderAmount} \nтовары:");
+                Console.WriteLine($"Номер заказа: {OrderCode} \nклиент: {Client}" +
+                    $" \nдата заказа: {_date.ToShortDateString()} \nСумма заказа: {OrderAmount} \nтовары:");
                 writeProducts(ref products);
             }
-            
+
             private void writeProducts(ref string[] products)
             {
                 foreach (var item in products)
@@ -33,43 +37,34 @@ namespace Request
             }
         }
 
-             
-        
-
-        
-
         private static void Main(string[] args)
         {
             Request request = new Request();
 
             Console.WriteLine("Введите код заказа");
-            request.orderCode = int.Parse(Console.ReadLine());
+            request.OrderCode = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Введите имя клиента");
-            request.client = Console.ReadLine();
+            request.Client = Console.ReadLine();
 
-            //Console.WriteLine("Введите дату заказа");
-            request.date = DateTime.Now;
+            request.Date = DateTime.Now;
 
             Console.WriteLine("Введите количество купленых продуктов");
             int order = int.Parse(Console.ReadLine());
 
-            // decimal sum = 0;
             request.products = new string[order];
             for (int i = 0; i < order; i++)
             {
-                Console.WriteLine($"Введите название {i+1} продукта");
+                Console.WriteLine($"Введите название {i + 1} продукта");
                 request.products[i] = Console.ReadLine();
 
-                Console.WriteLine($"Введите стоимость {i+1} продукта");
+                Console.WriteLine($"Введите стоимость {i + 1} продукта");
                 request.OrderAmount += decimal.Parse(Console.ReadLine());
             }
 
             Console.WriteLine("Вывожу данные");
 
             request.Print();
-
-            //Console.WriteLine(request.ToString()); 
 
             Console.ReadLine();
         }
